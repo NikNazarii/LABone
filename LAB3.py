f26de28bbd7dc29320d463606688e56eb8c40069
NikNazarii <n.nikonenko_fit_8_24_b_d@knute.edu.ua>
2024-10-11 20:16:32 +0300

    Add files via upload

---
+++ LAB3.py
@@ -0,0 +1,29 @@
+
+# VARIANT 16.1
+
+# a = input('Enter first number:'  )
+# b = input('Enter second number:'  )
+#
+# num1 = int(a+b)
+# num2 = int(b+a)
+#
+#
+# print("Result first: ",   num1   )
+# print("Result second: ",  num2   )
+
+
+# VARIANT 16.2
+
+# x1 = float(input("Введіть координату x1: "))
+# y1 = float(input("Введіть координату y1: "))
+#
+#
+# x2 = float(input("Введіть координату x2: "))
+# y2 = float(input("Введіть координату y2: "))
+#
+#
+# midle_x = (x1 + x2) / 2
+# midle_y = (y1 + y2) / 2
+#
+#
+# print("Координати середини відрізка: ({}, {})".format(midle_x, midle_y))
