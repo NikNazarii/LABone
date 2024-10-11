348f016b82c28ba74724770d8273da36de279710
NikNazarii <n.nikonenko_fit_8_24_b_d@knute.edu.ua>
2024-10-11 20:16:54 +0300

    Add files via upload

---
+++ LAB5.py
@@ -0,0 +1,35 @@
+# VARIANT 16.1
+import math
+
+# def f(x):
+#     if x >= 3:
+#          return x - math.log10(x) + math.log(2 * x) + pow(math.e, x)
+#     elif 0.2 < x < 3:
+#          return 6.53 * (1 - x) / pow(math.e, 0.1 * x) + 0.11 * x
+#     elif x <= 0.2:
+#          return 5.3 + x * pow(math.e, x)
+#     else:
+#         return None
+#
+# x_values = [0.1, 1, 2.5, 3, 4]
+# for x in x_values:
+#     print(f"f({x}) = {f(x)}")
+
+
+# VARIANT 16.2
+# num = int(input("Введіть трицифрове число: "))
+#
+#
+# first_num = num // 100
+# second_num = (num // 10) % 10
+# third_num = num % 10
+#
+#
+# max_num = max(first_num, second_num, third_num)
+# min_num = min(first_num, second_num, third_num)
+#
+#
+# sum = max_num + min_num
+#
+#
+# print("Сума найбільшої і найменшої цифр:", sum)
