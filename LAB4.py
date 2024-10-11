e0ed20ced2fd7e46888bdf5db35cff0a06c942c3
NikNazarii <n.nikonenko_fit_8_24_b_d@knute.edu.ua>
2024-10-11 20:16:43 +0300

    Add files via upload

---
+++ LAB4.py
@@ -0,0 +1,20 @@
+# VARIANT 16.1
+# import math
+#
+# x = float(input("Введіть значення x: "))
+#
+#
+# f_x = math.log(x) - math.log10(x) + math.log2(4 * x) / abs(math.cos(x)) + 0.4 + 0.12 * (math.sqrt(3 * x + 3) ** 5)
+#
+#
+# print("Значення функції f(x):", f_x)
+
+# VARIANT 16.2
+
+# x = float(input("Введіть значення x: "))
+# y = float(input("Введіть значення y: "))
+# z = float(input("Введіть значення z: "))
+#
+# M = abs(math.pow(math.sin(x), 2) - y * pow(math.e, 2*z)  ) + math.sqrt(abs(z)+2) - x + y / math.log(8) * abs(z)
+#
+# print("Значення функції M:", M)
